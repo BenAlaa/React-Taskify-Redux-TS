8583082f7704a1d354291139c2d80c0474ab1e9c
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-15 22:45:13 +0200

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Redirect, Switch } from "react-router-dom";
+import Main from './Components/Main/Main';
+import Logout from './Components/logout';
+import Login from './Components/Login/Login';
+import Profile,{ProfileProps} from "./Components/Profile/Profile";
+import Tasks, {TasksProps} from "./Components/Tasks/Tasks";
+import NavBar from './Components/NavBar/NavBar';
+import ProtectedRout from './Components/Common/ProtectedRout';
+import NotFound from './Components/NotFound/NotFound';
+import IUser from './Models/IUser';
+import {getCurrentUser} from './Services/authService';
+
 import './App.css';
 
 const App: React.FC = () => {
+    const user = getCurrentUser();
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar user={user}></NavBar>
+      <Switch>
+        <Route path="/main" component={Main}></Route>
+        <ProtectedRout path="/tasks" render={(props:TasksProps) => <Tasks {...props}/>} />
+        <Route path="/login" component={Login}></Route>
+        <Route path="/logout" component={Logout} />
+        <ProtectedRout path="/profile" render={(props:ProfileProps) => <Profile {...props}/>} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/main" />
+        <Redirect to="/not-found" />
+      </Switch>
+
+
+    </React.Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+// import {Provider} from 'react-redux';
+// import store from "./Redux/Store";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+import "font-awesome/css/font-awesome.min.css";
+// import "jquery";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    {/* <Provider store={store}>
+    </Provider> */}
+        <App />
+
+        
+    </BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
