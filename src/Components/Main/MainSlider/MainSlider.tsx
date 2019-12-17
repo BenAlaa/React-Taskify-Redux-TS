b46b2da1b357715a9bac38ea7cd72881e014ca47
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-17 09:15:40 +0200

    Edit Slider

--- src/Components/Main/MainSlider/MainSlider.tsx
+++ src/Components/Main/MainSlider/MainSlider.tsx
@@ -14,17 +14,18 @@ const MainSlider: React.SFC<MainSliderProps> = ({props}) => {
     }
     return ( 
         <div className="row slider">
+            
 
                 <div id="carouselExampleIndicators" className="carousel slide col-lg-12" data-ride="carousel">
                     
                     <div className="carousel-inner">
                         <div className="  carousel-item active slid-item item-one">
-                            <h1 className="Slid-item-quote col-lg-6 offset-lg-3">"The key is in not spending time, but in investing it." -- Stephen R. Covey</h1>
+                            <h1 className="Slid-item-quote col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">"The key is in not spending time, but in investing it." -- Stephen R. Covey</h1>
                             <button className="btn-start col-6 col-sm-3 col-md-2 col-lg-2 offset-3 offset-sm-5 offset-md-5" onClick={() => handleStart()}>Get Start</button>
 
                         </div>
                         <div className=" carousel-item slid-item item-two">
-                            <h1 className="Slid-item-quote col-lg-6 offset-lg-3">"Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin." - Mother Teresa</h1>
+                            <h1 className="Slid-item-quote col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">"Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin." - Mother Teresa</h1>
                             <button className="btn-start col-6 col-sm-3 col-md-2 col-lg-2 offset-3 offset-sm-5 offset-md-5" onClick={() => handleStart()}>Get Start</button>
 
 
